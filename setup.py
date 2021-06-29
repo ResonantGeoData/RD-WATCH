d52048439aa64b019d3e3826e10816c613b1f04a
banesullivan <bane.sullivan@kitware.com>
2021-06-29 13:04:31 -0600

    Bump RGD

--- setup.py
+++ setup.py
@@ -55,8 +55,8 @@ setup(
         'django-s3-file-field[boto3]',
         'gunicorn',
         # RGD
-        'django-rgd>=0.2.0rc8',
-        'django-rgd-imagery>=0.2.0rc8',
+        'django-rgd>=0.2.0',
+        'django-rgd-imagery>=0.2.0',
     ],
     extras_require={
         'dev': [
@@ -67,10 +67,10 @@ setup(
             'tox',
         ],
         'worker': [
-            'django-rgd-imagery[worker]>=0.2.0rc8',
+            'django-rgd-imagery[worker]>=0.2.0',
         ],
         'fuse': [
-            'django-rgd[fuse]>=0.2.0rc8',
+            'django-rgd[fuse]>=0.2.0',
         ],
     },
 )

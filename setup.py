ccfe386d55b6b92bd993e31f268b1ccc8ac90cae
banesullivan <bane.sullivan@kitware.com>
2021-12-09 12:17:59 -0700

    Bump RGD to remove kwcococ and kwimage depends

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ setup(
         'django-s3-file-field[boto3]',
         'gunicorn',
         # RGD
-        'django-rgd==0.2.8',
-        'django-rgd-imagery==0.2.8',
+        'django-rgd==0.2.9',
+        'django-rgd-imagery==0.2.9',
         'pandas',
     ],
     extras_require={
@@ -71,10 +71,10 @@ setup(
             'tox',
         ],
         'worker': [
-            'django-rgd-imagery[worker]==0.2.8',
+            'django-rgd-imagery[worker]==0.2.9',
         ],
         'fuse': [
-            'django-rgd[fuse]==0.2.8',
+            'django-rgd[fuse]==0.2.9',
         ],
     },
 )

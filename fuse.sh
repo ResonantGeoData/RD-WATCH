39e4436cdaf80f6d072cd0fbe65d48a0a71099a7
banesullivan <bane.sullivan@kitware.com>
2021-12-09 23:55:46 -0700

    Update fuse

--- fuse.sh
+++ fuse.sh
@@ -3,4 +3,6 @@ mkdir -p /tmp/rgd/https
 python -m simple_httpfs /tmp/rgd/https
 mkdir -p /tmp/rgd/http
 python -m simple_httpfs /tmp/rgd/http
+mkdir -p /tmp/rgd/s3
+python -m simple_httpfs /tmp/rgd/s3
 exec "$@"

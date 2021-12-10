6239d593affee02743c97aa6463e7be19afe36cd
banesullivan <bane.sullivan@kitware.com>
2021-12-09 19:22:22 -0700

    Fix STAC ingest

--- watch/core/tests/test_stac.py
+++ watch/core/tests/test_stac.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from rgd_imagery.large_image_utilities import yeild_tilesource_from_image
 
 from . import factories
 
@@ -21,6 +22,12 @@ def test_landsat_stac_support(sample_file):
     )
     stac_file.refresh_from_db()
     assert stac_file.raster
+    assert stac_file.raster.image_set.images.count()
+
+    # Make sure the image is reachable
+    image = stac_file.raster.image_set.images.first()
+    with yeild_tilesource_from_image(image) as src:
+        assert src.getMetadata()
 
 
 @pytest.mark.parametrize(
@@ -35,3 +42,9 @@ def test_sentinel_stac_support(sample_file):
     )
     stac_file.refresh_from_db()
     assert stac_file.raster
+    assert stac_file.raster.image_set.images.count()
+
+    # Make sure the image is reachable
+    image = stac_file.raster.image_set.images.first()
+    with yeild_tilesource_from_image(image) as src:
+        assert src.getMetadata()

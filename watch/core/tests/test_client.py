47848903898c7bbede98e75bcf1e6de8af6b5a6b
banesullivan <bane.sullivan@kitware.com>
2021-12-09 19:42:32 -0700

    Use real data with client tests

--- watch/core/tests/test_client.py
+++ watch/core/tests/test_client.py
@@ -1,16 +1,17 @@
+from pathlib import Path
 from typing import Dict
 
 from django.contrib.auth.models import User
-from django.core.files.uploadedfile import SimpleUploadedFile
 import pytest
 from rest_framework.authtoken.models import Token
-from rgd.datastore import datastore
 from rgd.models import ChecksumFile
-from rgd.models.mixins import Status
 from rgd_client import create_rgd_client
+from rgd_imagery.large_image_utilities import yeild_tilesource_from_image
 from rgd_watch_client import WATCHClient
 
 from watch.core.models import STACFile
+
+from . import factories
 
 
 @pytest.fixture
@@ -31,20 +32,13 @@ def py_client(live_server):
 
 @pytest.fixture
 def stac_file():
-    file_path = datastore.fetch('astro.png')
-    with open(file_path, 'rb') as file_contents:
-        stac_file: STACFile = STACFile.objects.create(
-            file=ChecksumFile.objects.create(
-                name='astro.png',
-                file=SimpleUploadedFile(name='astro.png', content=file_contents.read()),
-            ),
-        )
-
-    # Manually update status, so it isn't automatically set to QUEUED
-    stac_file.status = Status.SKIPPED
-    stac_file.save(update_fields=['status'])
-
-    return stac_file
+    file_path = Path(__file__).absolute().parent / 'data' / 'landsat-c2l1.json'
+    sfile = factories.STACFileFactory(
+        file__file__filename='landsat-c2l1.json',
+        file__file__from_path=file_path,
+    )
+    sfile.refresh_from_db()
+    return sfile
 
 
 def test_get_stac_file(py_client: WATCHClient, stac_file: STACFile):
@@ -60,8 +54,10 @@ def test_list_stac_file(py_client: WATCHClient, stac_file: STACFile):
 
 
 def test_post_stac_file(py_client: WATCHClient):
-    file_url = datastore.get_url('astro.png')
-    res: Dict = py_client.watch.post_stac_file(url=file_url, name='test')
+    file_url = 'https://landsatlook.usgs.gov/stac-server/collections/landsat-c2l1/items/LC08_L1GT_227122_20211204_20211204_02_T2'
+    res: Dict = py_client.watch.post_stac_file(
+        url=file_url, name='LC08_L1GT_227122_20211204_20211204_02_T2'
+    )
 
     # Assert checksum file created
     checksum_file_dict = res['file']
@@ -69,6 +65,16 @@ def test_post_stac_file(py_client: WATCHClient):
 
     # Assert stack file created
     assert STACFile.objects.filter(id=res['id']).exists()
+
+    # Assert imagery was ingested
+    stac_file = STACFile.objects.get(id=res['id'])
+    assert stac_file.raster
+    assert stac_file.raster.image_set.images.count()
+
+    # Make sure the image is reachable
+    image = stac_file.raster.image_set.images.first()
+    with yeild_tilesource_from_image(image) as src:
+        assert src.getMetadata()
 
 
 def test_reprocess_stac_file(py_client: WATCHClient, stac_file: STACFile):

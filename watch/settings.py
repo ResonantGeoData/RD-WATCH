d273691b90280c455befebbddd54a3f9e2ae2563
banesullivan <bane.sullivan@kitware.com>
2021-12-09 18:20:08 -0700

    Fix configuration method

--- watch/settings.py
+++ watch/settings.py
@@ -21,7 +21,7 @@ class WatchMixin(ResonantGeoDataBaseMixin, ConfigMixin):
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
     @staticmethod
-    def before_binding(configuration: ComposedConfiguration) -> None:
+    def mutate_configuration(configuration: ComposedConfiguration) -> None:
         # Install local apps first, to ensure any overridden resources are found first
         configuration.INSTALLED_APPS = [
             'watch.core.apps.CoreConfig',

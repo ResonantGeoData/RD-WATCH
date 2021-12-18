f3380011eade88b5e3dabcdb0f27c25f3d760145
banesullivan <bane.sullivan@kitware.com>
2021-12-17 18:48:25 -0700

    Iterate directly over Landsat S3 bucket

--- scripts/landsat.py
+++ scripts/landsat.py
@@ -1,19 +1,17 @@
-from datetime import datetime
-
-from watch_helpers import post_stac_items_from_server
+from watch_helpers import post_stac_items_from_s3_iter
 
 if __name__ == '__main__':
-    min_date = datetime(2013, 1, 1)  # Arbitrarily chosen
-    max_date = datetime.today()
-    host_url = 'https://landsatlook.usgs.gov/stac-server/'
-
-    collection = 'landsat-c2l1'
-    print(collection)
-    post_stac_items_from_server(host_url, collection, min_date=min_date, max_date=max_date)
-
-    collection = 'landsat-c2l2-sr'
-    print(collection)
-    post_stac_items_from_server(host_url, collection, min_date=min_date, max_date=max_date)
+    # min_date = datetime(2013, 1, 1)  # Arbitrarily chosen
+    # max_date = datetime.today()
+    # host_url = 'https://landsatlook.usgs.gov/stac-server/'
+    #
+    # collection = 'landsat-c2l1'
+    # print(collection)
+    # post_stac_items_from_server(host_url, collection, min_date=min_date, max_date=max_date)
+    #
+    # collection = 'landsat-c2l2-sr'
+    # print(collection)
+    # post_stac_items_from_server(host_url, collection, min_date=min_date, max_date=max_date)
 
     # host_url = 'https://api.smart-stac.com/'
     # collection = 'smart-landsat-c2l1'
@@ -21,3 +19,17 @@ if __name__ == '__main__':
     #
     # collection = 'smart-landsat-c2l2-sr'
     # post_stac_items_from_server(host_url, collection, min_date=min_date, max_date=max_date)
+
+    bucket = 'usgs-landsat'
+    region = 'us-west-2'
+    include_regex: str = r'^.*stac\.json'
+
+    prefix = 'collection02/level-1/'
+    collection = 'landsat-c2l1'
+    print(collection)
+    post_stac_items_from_s3_iter(bucket, prefix, collection, include_regex=include_regex, region=region)
+
+    prefix = 'collection02/level-2/'
+    collection = 'landsat-c2l2-sr'
+    print(collection)
+    post_stac_items_from_s3_iter(bucket, prefix, collection, include_regex=include_regex, region=region)

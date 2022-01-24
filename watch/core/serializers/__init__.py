99a9aeb08b712d960e50b8fa11b1efc4884b575d
banesullivan <bane.sullivan@kitware.com>
2022-01-23 23:25:47 -0700

    linting

--- watch/core/serializers/__init__.py
+++ watch/core/serializers/__init__.py
@@ -2,4 +2,4 @@ from .item import ItemSerializer
 from .region import RegionSerializer
 from .stacfile import StacFileGetSerializer, STACFileSerializer
 
-__all__ = ['ItemSerializer', 'RegionSerializer', 'STACFileSerializer']
+__all__ = ['ItemSerializer', 'RegionSerializer', 'STACFileSerializer', 'StacFileGetSerializer']

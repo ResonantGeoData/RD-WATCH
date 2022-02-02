03271f91a97854ad3405e0747355863d0753a281
banesullivan <bane.sullivan@kitware.com>
2022-02-02 08:20:49 -0700

    Ingest more regions

--- scripts/regions.py
+++ scripts/regions.py
@@ -2,15 +2,28 @@ from watch_helpers import post_region_items_from_s3_iter
 
 if __name__ == '__main__':
     # s3://kitware-smart-watch-data/scratch/kw/proposed_annotations_sample/
+    # post_region_items_from_s3_iter(
+    #     bucket='kitware-smart-watch-data',
+    #     prefix='scratch/kw/proposed_annotations_sample/regions',
+    #     dry_run=False,
+    #     sites=False,
+    # )
+    # post_region_items_from_s3_iter(
+    #     bucket='kitware-smart-watch-data',
+    #     prefix='scratch/kw/proposed_annotations_sample/sites',
+    #     dry_run=False,
+    #     sites=True,
+    # )
+    # s3://smart-imagery/annotations/
     post_region_items_from_s3_iter(
-        bucket='kitware-smart-watch-data',
-        prefix='scratch/kw/proposed_annotations_sample/regions',
+        bucket='smart-imagery',
+        prefix='region_models',
         dry_run=False,
         sites=False,
     )
     post_region_items_from_s3_iter(
-        bucket='kitware-smart-watch-data',
-        prefix='scratch/kw/proposed_annotations_sample/sites',
+        bucket='smart-imagery',
+        prefix='site_models/',
         dry_run=False,
         sites=True,
     )

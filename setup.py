c76d952dfef01a36c7ae0a3bb2c84e9938c53c26
banesullivan <bane.sullivan@kitware.com>
2022-03-02 13:14:58 -0700

    pin deps in reqs.txt

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup(
         'django-s3-file-field[boto3]',
         'gunicorn',
         # RGD
-        'django-rgd==0.2.18',
-        'django-rgd-imagery==0.2.18',
+        'django-rgd',
+        'django-rgd-imagery',
     ],
     extras_require={
         'dev': [
@@ -71,10 +71,10 @@ setup(
             'tox',
         ],
         'worker': [
-            'django-rgd-imagery[worker]==0.2.18',
+            'django-rgd-imagery[worker]',
         ],
         'fuse': [
-            'django-rgd[fuse]==0.2.18',
+            'django-rgd[fuse]',
         ],
     },
 )

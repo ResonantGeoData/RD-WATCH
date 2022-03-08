ef772114c4c32424c0530546ee675fd081ec4902
banesullivan <bane.sullivan@kitware.com>
2022-03-08 16:20:51 -0700

    Fix bad test data

--- watch/core/tests/test_client.py
+++ watch/core/tests/test_client.py
@@ -55,10 +55,10 @@ def test_list_stac_file(py_client: WATCHClient, stac_file: STACFile):
 
 
 def test_post_stac_file(py_client: WATCHClient):
-    file_url = 'https://landsatlook.usgs.gov/stac-server/collections/landsat-c2l1/items/LC08_L1GT_227122_20211204_20211204_02_T2'  # noqa
+    file_url = 'https://landsatlook.usgs.gov/stac-server/collections/landsat-c2l1/items/LC09_L1GT_052212_20220308_20220308_02_T2'  # noqa
     res: Dict = py_client.watch.post_stac_file(
         url=file_url,
-        name='LC08_L1GT_227122_20211204_20211204_02_T2',
+        name='LC09_L1GT_052212_20220308_20220308_02_T2',
         collection=None,
     )
 

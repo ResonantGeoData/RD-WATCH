2f31751c3908994be686ebe05e2136a687e2081c
Matt Covalt <matt@kiwiziti.com>
2022-08-09 18:41:45 -0400

    Make dummy test

--- vue/test/client.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe("import vue components", () => {
-  test("normal imports as expected", async () => {
-    const cmp = await import("../src/App.vue");
-    expect(cmp).toBeDefined();
-  });
-});
---
+++ vue/test/testing.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("test testing", () => {
+  test("testing framework", async () => {
+    expect(true).toBeTruthy();
+  });
+});

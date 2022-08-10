82c399612375fb99a7d54f32585928929dc5fddb
Matt Covalt <matt@kiwiziti.com>
2022-08-10 15:41:30 -0400

    Add help text and constraints to each model

--- django/src/rdwatch/models/prediction_configuration.py
+++ django/src/rdwatch/models/prediction_configuration.py
@@ -2,4 +2,9 @@ from django.contrib.gis.db import models
 
 
 class PredictionConfiguration(models.Model):
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(
+        help_text="Time when evaluating this prediction configuration was finished"
+    )
+
+    class Meta:
+        indexes = [models.Index(fields=["timestamp"])]

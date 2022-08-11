5d579e637a3a40db2717973b42a469b79c5eea93
Matt Covalt <matt@kiwiziti.com>
2022-08-11 06:55:29 -0400

    Add Site view

--- django/src/rdwatch/urls.py
+++ django/src/rdwatch/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from rdwatch import views
 from rest_framework.renderers import CoreJSONRenderer
+from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r"site", views.SiteViewSet)
 
 urlpatterns = [
     path(
@@ -15,4 +20,5 @@ urlpatterns = [
         name="openapi-schema",
     ),
     path("status", views.RetrieveServerStatus.as_view()),
+    path("", include(router.urls)),
 ]

8b5fef45e03f9bc512983d7898127a7fc3532525
Matt Covalt <matt@kiwiziti.com>
2022-08-22 15:47:27 -0400

    Add view to get saliency tiles

--- django/src/rdwatch/views/__init__.py
+++ django/src/rdwatch/views/__init__.py
@@ -1,5 +1,11 @@
+from .saliency_tile import RetrieveSaliencyTile
 from .server_status import RetrieveServerStatus
 from .site import SiteViewSet
 from .site_tile import RetrieveSiteTile
 
-__all__ = ["RetrieveServerStatus", "SiteViewSet", "RetrieveSiteTile"]
+__all__ = [
+    "RetrieveServerStatus",
+    "SiteViewSet",
+    "RetrieveSiteTile",
+    "RetrieveSaliencyTile",
+]

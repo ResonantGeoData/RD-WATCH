f23279dc82bc2ddae6b95e4fa6e9e535866b2daf
Matt Covalt <matt@kiwiziti.com>
2022-09-21 11:41:17 -0400

    Add `TimeSlider` component

---
+++ vue/src/components/TimeSlider.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { state } from "../store";
+
+defineProps<{
+  min: number;
+  max: number;
+}>();
+</script>
+
+<template>
+  <input
+    v-model.number="state.timestamp"
+    :min="min"
+    :max="max"
+    :disabled="min == max"
+    class="w-full"
+    type="range"
+  />
+</template>

94677bbb5a6cbd860b2af40a0022f83a85000ec6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-11-02 18:15:19 +0000

    Set default pagination class and page size for DRF

--- django/src/rdwatch/server/settings.py
+++ django/src/rdwatch/server/settings.py
@@ -44,6 +44,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "DEFAULT_PARSER_CLASSES": ["rest_framework.parsers.JSONParser"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
 }
 ROOT_URLCONF = "rdwatch.server.urls"
 USE_I18N = False

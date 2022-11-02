c372d8093396fd29a63e9310efe1af5ab1981d37
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-11-02 18:16:49 +0000

    Paginate site evaluations endpoint

--- django/src/rdwatch/views/site_evaluation.py
+++ django/src/rdwatch/views/site_evaluation.py
@@ -2,12 +2,14 @@ import django_filters
 
 from django.contrib.gis.db.models.aggregates import Collect
 from django.contrib.postgres.aggregates import JSONBAgg
+from django.core.paginator import Paginator
 from django.db.models import Count, Max, Min
 from django.db.models.functions import JSONObject  # type: ignore
 from django.http import HttpRequest
 from rest_framework.decorators import api_view, schema
 from rest_framework.response import Response
 from rest_framework.schemas.openapi import AutoSchema
+from rest_framework.settings import api_settings
 
 from rdwatch.db.functions import BoundingBox, ExtractEpoch
 from rdwatch.models import SiteEvaluation
@@ -37,47 +39,72 @@ class SiteEvaluationsSchema(AutoSchema):
 @api_view(["GET"])
 @schema(SiteEvaluationsSchema())
 def site_evaluations(request: HttpRequest):
-    queryset = (
-        SiteEvaluationsFilter(
-            request.GET, queryset=SiteEvaluation.objects.order_by("-timestamp")
-        )
-        .qs.annotate(
-            timemin=Min("observations__timestamp"),
-            timemax=Max("observations__timestamp"),
-        )
-        .aggregate(
-            count=Count("pk"),
-            timerange=JSONObject(
-                min=ExtractEpoch(Min("timemin")),
-                max=ExtractEpoch(Max("timemax")),
-            ),
-            bbox=BoundingBox(Collect("geom")),
-            results=JSONBAgg(
-                JSONObject(
-                    id="pk",
-                    site=JSONObject(
-                        region=JSONObject(
-                            country="region__country",
-                            classification="region__classification__slug",
-                            number="region__number",
-                        ),
-                        number="number",
+    queryset = SiteEvaluationsFilter(
+        request.GET,
+        queryset=SiteEvaluation.objects.order_by("-timestamp"),
+    ).qs
+
+    # Pagination
+    assert api_settings.PAGE_SIZE, "PAGE_SIZE must be set."
+    paginator = Paginator(queryset, api_settings.PAGE_SIZE)
+    page = paginator.page(request.GET.get("page", 1))
+    queryset = page.object_list  # get the paginated queryset
+
+    queryset = queryset.annotate(
+        timemin=Min("observations__timestamp"),
+        timemax=Max("observations__timestamp"),
+    ).aggregate(
+        count=Count("pk"),
+        timerange=JSONObject(
+            min=ExtractEpoch(Min("timemin")),
+            max=ExtractEpoch(Max("timemax")),
+        ),
+        bbox=BoundingBox(Collect("geom")),
+        results=JSONBAgg(
+            JSONObject(
+                id="pk",
+                site=JSONObject(
+                    region=JSONObject(
+                        country="region__country",
+                        classification="region__classification__slug",
+                        number="region__number",
                     ),
-                    configuration="configuration__parameters",
-                    performer=JSONObject(
-                        team_name="configuration__performer__description",
-                        short_code="configuration__performer__slug",
-                    ),
-                    score="score",
-                    bbox=BoundingBox("geom"),
-                    timestamp=ExtractEpoch("timestamp"),
-                    timerange=JSONObject(
-                        min=ExtractEpoch("timemin"),
-                        max=ExtractEpoch("timemax"),
-                    ),
-                )
-            ),
-        )
+                    number="number",
+                ),
+                configuration="configuration__parameters",
+                performer=JSONObject(
+                    team_name="configuration__performer__description",
+                    short_code="configuration__performer__slug",
+                ),
+                score="score",
+                bbox=BoundingBox("geom"),
+                timestamp=ExtractEpoch("timestamp"),
+                timerange=JSONObject(
+                    min=ExtractEpoch("timemin"),
+                    max=ExtractEpoch("timemax"),
+                ),
+            )
+        ),
     )
+
+    # Set `next` and `prev` response values for pagination
+    if page.has_next():
+        next_page_query_params = request.GET.copy()
+        next_page_query_params["page"] = str(page.next_page_number())
+        queryset[
+            "next"
+        ] = f"{request.get_full_path()}?{next_page_query_params.urlencode()}"
+    else:
+        queryset["next"] = None
+
+    if page.has_previous():
+        prev_page_query_params = request.GET.copy()
+        prev_page_query_params["page"] = str(page.previous_page_number())
+        queryset[
+            "previous"
+        ] = f"{request.get_full_path()}?{prev_page_query_params.urlencode()}"
+    else:
+        queryset["previous"] = None
+
     serializer = SiteEvaluationListSerializer(queryset)
     return Response(serializer.data)

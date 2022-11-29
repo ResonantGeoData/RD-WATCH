2e1a2937100efc427bf76e32cca21e582c4fe3df
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-11-29 16:45:48 -0500

    Add type annotation for the store

--- vue/src/store.ts
+++ vue/src/store.ts
@@ -1,6 +1,10 @@
 import { reactive } from "vue";
 
-export const state = reactive({
+export const state = reactive<{
+  timestamp: number;
+  bbox: { xmin: number; ymin: number; xmax: number; ymax: number };
+  filters: Record<string, string>;
+}>({
   timestamp: Math.floor(Date.now() / 1000),
   bbox: {
     xmin: -180,

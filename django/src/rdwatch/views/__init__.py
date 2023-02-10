c72ce95b55d98b9f8955264cae60d44a7e5a31e4
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-02-10 14:16:32 -0500

    site-summary ingest endpoint

--- django/src/rdwatch/views/__init__.py
+++ django/src/rdwatch/views/__init__.py
@@ -3,7 +3,7 @@ from .performer import PerformerViewSet
 from .region import RegionViewSet
 from .server_status import RetrieveServerStatus
 from .site_evaluation import site_evaluations
-from .site_model import post_site_model
+from .site_model import post_site_model, post_site_summaries
 from .site_observation import site_observations
 from .tile import (
     satelliteimage_raster_tile,
@@ -20,6 +20,7 @@ __all__ = [
     "RetrieveServerStatus",
     "site_evaluations",
     "post_site_model",
+    "post_site_summaries",
     "site_observations",
     "satelliteimage_raster_tile",
     "satelliteimage_time_list",

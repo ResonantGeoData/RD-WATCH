c03d44e9994d16e5c6f469efd75d6899b65c0df1
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2023-03-17 15:09:05 -0400

    improve satellite loading toggle (#162)

--- vue/src/interactions/satelliteLoading.ts
+++ vue/src/interactions/satelliteLoading.ts
@@ -1,12 +1,16 @@
 import {  Map, MapDataEvent } from "maplibre-gl";
-import { ShallowRef } from "vue";
+import { ShallowRef, watch } from "vue";
 import { state } from "../store";
 
 
-const satelliteTilesUIDs: Record<number, string> = {};
+const satelliteTilesUIDs: Record<string, string> = {};
 type MapDataLoad = MapDataEvent & { sourceId?: string, tile? : { uid?: number}}
 
 const satelliteLoading = (map: ShallowRef<null | Map>) => {
+    watch([() => state.satellite.satelliteTimeStamp, () => state.satellite.satelliteImagesOn], () => {
+        state.satellite.loadingSatelliteImages = false;
+        Object.keys(satelliteTilesUIDs).forEach((key) => delete satelliteTilesUIDs[key]);
+    })
     if (map.value) {
         map.value.on("data", function (e: MapDataLoad) {
         if (e.sourceId === 'satelliteTiles') {

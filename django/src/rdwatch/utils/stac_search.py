aa259706de60d9140e7ae6abb635cd81ba303005
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2023-04-11 11:36:15 -0400

    Use SMART Program STAC server for S2/L8, make Accenture version configurable w/ env var (#172)

--- django/src/rdwatch/utils/stac_search.py
+++ django/src/rdwatch/utils/stac_search.py
@@ -1,7 +1,10 @@
 import json
 from datetime import datetime, timedelta
+from os import environ, path
 from typing import Literal, TypedDict
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+
+from django.conf import settings
 
 
 class EOBand(TypedDict, total=False):
@@ -53,35 +56,25 @@ def _fmt_time(time: datetime):
     return f'{time.isoformat()[:19]}Z'
 
 
-def landsat_search(
+COLLECTIONS: dict[str, list[str]] = {
+    'L8': ['landsat-c2l1', 'landsat-c2l2-sr'],
+    'S2': [],
+}
+
+if settings.ACCENTURE_IMAGERY_VERSION is not None:
+    COLLECTIONS['S2'].append(f'ta1-s2-acc-{settings.ACCENTURE_IMAGERY_VERSION}')
+
+
+def stac_search(
+    source: Literal['S2', 'L8'],
     timestamp: datetime,
     bbox: tuple[float, float, float, float],
     timebuffer: timedelta | None = None,
     page: int = 1,
 ) -> Results:
-    url = 'https://landsatlook.usgs.gov/stac-server/search'
-    if timebuffer is not None:
-        min_time = timestamp - timebuffer
-        max_time = timestamp + timebuffer
-        time_str = f'?datetime={_fmt_time(min_time)}/{_fmt_time(max_time)}'
-    else:
-        time_str = f'?datetime={_fmt_time(timestamp)}'
-    url += time_str
-    url += f"&bbox={','.join(str(coord) for coord in bbox)}"
-    url += '&collections=landsat-c2l1,landsat-c2l2-sr'
-    url += f'&page={page}'
-    url += '&limit=100'
-    with urlopen(url) as resp:
-        return json.loads(resp.read())
-
-
-def sentinel_search(
-    timestamp: datetime,
-    bbox: tuple[float, float, float, float],
-    timebuffer: timedelta | None = None,
-    page: int = 1,
-) -> Results:
-    url = 'https://earth-search.aws.element84.com/v0/search'
+    # Use SMART program server instead of public server
+    # (https://earth-search.aws.element84.com/v0/search)
+    url = path.join(environ['RDWATCH_SMART_STAC_URL'], 'search')
     params = SearchParams()
     params['bbox'] = bbox
     if timebuffer is not None:
@@ -91,12 +84,13 @@ def sentinel_search(
     else:
         time_str = f'{_fmt_time(timestamp)}Z'
     params['datetime'] = time_str
-    params['collections'] = [
-        'sentinel-s2-l1c',
-        'sentinel-s2-l2a',
-        'sentinel-s2-l2a-cogs',
-    ]
+    params['collections'] = COLLECTIONS[source]
     params['page'] = page
     params['limit'] = 100
-    with urlopen(url, bytes(json.dumps(params), 'utf-8')) as resp:
+    request = Request(
+        url,
+        data=bytes(json.dumps(params), 'utf-8'),
+        headers={'x-api-key': environ['RDWATCH_SMART_STAC_KEY']},
+    )
+    with urlopen(request) as resp:
         return json.loads(resp.read())

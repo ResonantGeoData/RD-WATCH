16888a0ef24648991c3a633744686d98066c6c41
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-04-21 09:27:53 -0400

    setting up new table

---
+++ django/src/rdwatch/models/site_image.py
@@ -0,0 +1,39 @@
+from django.contrib.postgres.indexes import GistIndex
+from django.db import models
+from django.dispatch import receiver
+
+
+class SiteImage(models.Model):
+    siteeval = models.ForeignKey(
+        to='SiteEvaluation',
+        on_delete=models.CASCADE,
+        help_text='The site evaluation associated with this observation.',
+        db_index=True,
+    )
+    siteobs = models.ForeignKey(
+        to='SiteObservation',
+        on_delete=models.CASCADE,
+        help_text='The site Observation associated with this Image if it exists.',
+        null=True,
+        db_index=True,
+    )
+    timestamp = models.DateTimeField(
+        help_text="The source image's timestamp",
+    )
+    image = models.FileField(null=True, blank=True)
+    cloudcover = models.FloatField(
+        null=True, help_text='Cloud Cover associated with Image'
+    )
+    source = models.CharField(
+        max_length=2, blank=True, help_text='WV, S2, L8 imagery source'
+    )
+
+    class Meta:
+        default_related_name = 'siteimages'
+        indexes = [GistIndex(fields=['timestamp'])]
+
+
+@receiver(models.signals.pre_delete, sender=SiteImage)
+def delete_content(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(save=False)

da49cac5148dd0a118cc47466f852de8617371b4
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-04-26 13:35:20 -0400

    Use django-configurations for settings.py

--- django/src/rdwatch/server/__init__.py
+++ django/src/rdwatch/server/__init__.py
@@ -1,7 +1,12 @@
 import os
 
+import configurations.importer
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rdwatch.server.settings')
+if not os.environ.get('DJANGO_CONFIGURATION'):
+    raise ValueError('The environment variable "DJANGO_CONFIGURATION" must be set.')
+configurations.importer.install()
 
 application = get_wsgi_application()
--- django/src/rdwatch/server/settings.py
+++ django/src/rdwatch/server/settings.py
@@ -1,63 +1,83 @@
-from os import environ
-from urllib.parse import urlparse
+import os
 
-SECRET_KEY = environ['RDWATCH_SECRET_KEY']
+from configurations import Configuration, values
+
+_environ_prefix = 'RDWATCH'
 
 
-ALLOWED_HOSTS = ['*']
-DEBUG = environ['RDWATCH_DJANGO_DEBUG'].lower() in ('1', 'true', 'yes', 'on')
+class BaseConfiguration(Configuration):
+    ROOT_URLCONF = 'rdwatch.server.urls'
+    USE_I18N = False
+    USE_TZ = False
+    USE_X_FORWARDED_HOST = True
+    WSGI_APPLICATION = 'rdwatch.server.application'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': environ['RDWATCH_REDIS_URI'],
-        'OPTIONS': {
-            'CLIENT_CLASS': 'rdwatch.redis.CustomRedisCluster',
-        },
+    SECRET_KEY = values.Value(environ_required=True, environ_prefix=_environ_prefix)
+
+    INSTALLED_APPS = [
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.gis',
+        'django.contrib.postgres',
+        'django_filters',
+        'rest_framework',
+        'django_extensions',
+        'rdwatch',
+    ]
+
+    MIDDLEWARE = [
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'rdwatch.middleware.Log500ErrorsMiddleware',
+    ]
+
+    REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+        'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser'],
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 25,
     }
-}
 
-DATABASE_PARSE_RESULT = urlparse(environ['RDWATCH_POSTGRESQL_URI'])
-DATABASES = {
-    'default': {
-        'CONN_MAX_AGE': None,
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'HOST': DATABASE_PARSE_RESULT.hostname,
-        'NAME': DATABASE_PARSE_RESULT.path.strip('/'),
-        'PASSWORD': DATABASE_PARSE_RESULT.password,
-        'PORT': DATABASE_PARSE_RESULT.port,
-        'USER': DATABASE_PARSE_RESULT.username,
-        'TEST': {
-            'NAME': 'test_rdwatch',
-        },
-    },
-}
-CELERY_BROKER_URL = environ['RDWATCH_CELERY_BROKER_URL']
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.gis',
-    'django.contrib.postgres',
-    'django_filters',
-    'rest_framework',
-    'django_extensions',
-    'rdwatch',
-]
-MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rdwatch.middleware.Log500ErrorsMiddleware',
-]
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
-    'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25,
-}
-ROOT_URLCONF = 'rdwatch.server.urls'
-USE_I18N = False
-USE_TZ = False
-USE_X_FORWARDED_HOST = True
-WSGI_APPLICATION = 'rdwatch.server.application'
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': os.environ['RDWATCH_REDIS_URI'],
+            'OPTIONS': {
+                'CLIENT_CLASS': 'rdwatch.redis.CustomRedisCluster',
+            },
+        }
+    }
 
-ACCENTURE_IMAGERY_VERSION = environ.get('RDWATCH_ACCENTURE_VERSION', None)
+    CELERY_BROKER_URL = values.Value(
+        environ_required=True,
+        environ_prefix=_environ_prefix,
+    )
+
+    DATABASES = values.DatabaseURLValue(
+        environ_name='POSTGRESQL_URI',
+        environ_prefix=_environ_prefix,
+        environ_required=True,
+        # Additional kwargs to DatabaseURLValue are passed to dj-database-url
+        engine='django.contrib.gis.db.backends.postgis',
+        conn_max_age=None,
+    )
+
+    ACCENTURE_VERSION = values.Value(
+        environ_required=True, environ_prefix=_environ_prefix
+    )
+
+
+class DevelopmentConfiguration(BaseConfiguration):
+    DEFAULT_FILE_STORAGE = 'minio_storage.storage.MinioMediaStorage'
+    MINIO_STORAGE_ENDPOINT = values.Value(
+        'localhost:9000', environ_prefix=_environ_prefix
+    )
+    MINIO_STORAGE_USE_HTTPS = values.BooleanValue(False, environ_prefix=_environ_prefix)
+    MINIO_STORAGE_ACCESS_KEY = values.SecretValue(environ_prefix=_environ_prefix)
+    MINIO_STORAGE_SECRET_KEY = values.SecretValue(environ_prefix=_environ_prefix)
+    MINIO_STORAGE_MEDIA_BUCKET_NAME = values.Value(
+        environ_prefix=_environ_prefix, environ_required=True
+    )
+    MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
+    MINIO_STORAGE_AUTO_CREATE_MEDIA_POLICY = 'READ_WRITE'
+    MINIO_STORAGE_MEDIA_USE_PRESIGNED = True

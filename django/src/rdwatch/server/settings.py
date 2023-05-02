34f256b73dcba9d6101bbfcd2d6b510a96752e0d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-05-01 22:34:36 -0400

    Add production configuration using S3

--- django/src/rdwatch/server/settings.py
+++ django/src/rdwatch/server/settings.py
@@ -5,6 +5,13 @@ from configurations import Configuration, values
 _environ_prefix = 'RDWATCH'
 
 
+# With the default "late_binding=False", and "environ_name" is
+# specified or "environ=False", even Values from non-included
+# classes (e.g. `AWS_DEFAULT_REGION) get immediately evaluated and
+# expect env vars to be set.
+values.Value.late_binding = True
+
+
 class BaseConfiguration(Configuration):
     ROOT_URLCONF = 'rdwatch.server.urls'
     USE_I18N = False
@@ -81,3 +88,38 @@ class DevelopmentConfiguration(BaseConfiguration):
     MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
     MINIO_STORAGE_AUTO_CREATE_MEDIA_POLICY = 'READ_WRITE'
     MINIO_STORAGE_MEDIA_USE_PRESIGNED = True
+
+
+class ProductionConfiguration(BaseConfiguration):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # This exact environ_name is important, as direct use of Boto will also use it
+    AWS_S3_REGION_NAME = values.Value(
+        environ_prefix=_environ_prefix,
+        environ_name='AWS_DEFAULT_REGION',
+        environ_required=True,
+    )
+    AWS_S3_ACCESS_KEY_ID = values.Value(
+        environ_prefix=_environ_prefix,
+        environ_name='AWS_ACCESS_KEY_ID',
+        environ_required=True,
+    )
+    AWS_S3_SECRET_ACCESS_KEY = values.Value(
+        environ_prefix=_environ_prefix,
+        environ_name='AWS_SECRET_ACCESS_KEY',
+        environ_required=True,
+    )
+
+    AWS_STORAGE_BUCKET_NAME = values.Value(
+        _environ_prefix=_environ_prefix,
+        environ_name='STORAGE_BUCKET_NAME',
+        environ_required=True,
+    )
+
+    # It's critical to use the v4 signature;
+    # it isn't the upstream default only for backwards compatability reasons.
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+    AWS_S3_MAX_MEMORY_SIZE = 5 * 1024 * 1024
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_QUERYSTRING_EXPIRE = 3600 * 6  # 6 hours

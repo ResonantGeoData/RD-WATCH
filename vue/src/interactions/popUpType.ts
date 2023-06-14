996923c30f178ea76ff390c7c61c967f420398cd
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-06-14 13:30:09 -0400

    updating popupData type

--- vue/src/interactions/popUpType.ts
+++ vue/src/interactions/popUpType.ts
@@ -1,3 +1,5 @@
+import { ScoringResults } from "../client/services/ApiService";
+
 export interface PopUpData {
     siteId: string;
     siteColor: string;
@@ -5,4 +7,7 @@ export interface PopUpData {
     groundTruth: boolean;
     scoreColor: string;
     area: string;
+    annotatedStatus?: ScoringResults['statusAnnotated'];
+    unionArea?: ScoringResults['unionArea'];
+    temporalIOU?: ScoringResults['temporalIOU'];  
   }  

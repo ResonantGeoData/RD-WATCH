bb85325f9b7a0fb16c0bcdb1c1413a711aa0d7d0
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2023-06-27 11:05:43 -0400

    Update django/src/rdwatch_scoring/urls.py
    Co-authored-by: Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>

--- django/src/rdwatch_scoring/urls.py
+++ django/src/rdwatch_scoring/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from rest_framework import routers
 
 from rdwatch_scoring.api import api
-
-router = routers.SimpleRouter(trailing_slash=False)
 
 urlpatterns = [
     path('/api/scoring', api.urls),
 ]
-
-urlpatterns += router.urls

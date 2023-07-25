e639c743df9d6d7bbb3ca4d4257211b947a7eabf
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-07-25 08:15:17 -0400

    lint

--- django/src/rdwatch/api.py
+++ django/src/rdwatch/api.py
@@ -3,8 +3,8 @@ from ninja import NinjaAPI
 from .views.model_run import router as model_run_router
 from .views.performer import router as performer_router
 from .views.region import router as region_router
-from .views.site_image import router as images_router
 from .views.site_evaluation import router as site_evaluation_router
+from .views.site_image import router as images_router
 
 api = NinjaAPI()
 

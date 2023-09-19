d31b5b9d8914f464529cb96ffc32cecb40a34d05
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2023-09-19 14:40:44 -0400

    Add `django-debug-toolbar` as a dev dependency (#263)

--- django/src/rdwatch/server/urls.py
+++ django/src/rdwatch/server/urls.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,6 +7,17 @@ urlpatterns = [
     path('api/', include('rdwatch.urls')),
     path('admin/', admin.site.urls),
 ]
+
 # Conditionally add the scoring URLs if the scoring app is installed
 if 'rdwatch_scoring' in apps.app_configs.keys():
     urlpatterns.append(path('api/', include('rdwatch_scoring.urls')))
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+
+        urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+    except ModuleNotFoundError:
+        # If DEBUG mode is enabled in production, django-debug-toolbar is
+        # likely not installed. If this happens, just ignore the error.
+        pass

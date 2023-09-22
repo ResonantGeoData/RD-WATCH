467bc950b99785411e9dade82c373367f40fbaba
Wang, Sean S <sean.wang@jhuapl.edu>
2023-09-22 10:38:15 -0400

    Added proposal endpoint and added scoring mode for loading model runs

--- django/src/rdwatch_scoring/api.py
+++ django/src/rdwatch_scoring/api.py
@@ -1,9 +1,11 @@
 from ninja import NinjaAPI
+from ninja.errors import ValidationError
 
 from .views.model_run import router as modelruns
 from .views.performer import router as performers
 from .views.region import router as regions
 from .views.scores import router as scores
+from .views.proposal import router as proposal_router
 
 api = NinjaAPI(urls_namespace='scoring')
 
@@ -11,3 +13,10 @@ api.add_router('/scoring/scores/', scores)
 api.add_router('/scoring/performers/', performers)
 api.add_router('/scoring/regions/', regions)
 api.add_router('/scoring/model-runs/', modelruns)
+api.add_router('/scoring/evaluations/', proposal_router)
+
+# for getting info about validation errors
+@api.exception_handler(ValidationError)
+def custom_validation_errors(request, exc):
+    print(exc.errors)  # <--------------------- !!!!
+    return api.create_response(request, {'detail': exc.errors}, status=422)
--- scripts/loadModelRun.py
+++ scripts/loadModelRun.py
@@ -7,7 +7,7 @@ from glob import glob
 import requests
 
 # TODO: Parameterize
-rgd_endpoint = "http://localhost:8000"
+rgd_endpoint = "http://localhost:8080"
 
 
 def main():
@@ -44,7 +44,7 @@ def main():
     parser.add_argument(
         "--expiration_time", default=None, type=int, help="expiration time in hours"
     )
-    upload_to_rgd(**vars(parser.parse_args()))
+    upload_to_rgd_scoring(**vars(parser.parse_args()))
 
 
 def upload_to_rgd(
@@ -140,6 +140,86 @@ def post_site(post_site_url, site_filepath, rgd_auth_cookie):
         )
 
     return response
+
+
+def upload_to_rgd_scoring(
+    region_id,
+    site_models_glob,
+    rgd_auth_cookie,
+    title="Ground Truth",
+    performer_shortcode="TE",
+    proposal=None,
+    eval_num=None,
+    eval_run_num=None,
+    expiration_time=None,
+):
+    # Check that our run doesn't already exist
+    model_run_results_url = f"{rgd_endpoint}/api/scoring/model-runs/"
+    cookies = None
+    if rgd_auth_cookie:
+        cookies = {"AWSELBAuthSessionCookie-0": rgd_auth_cookie}
+    model_runs_result = requests.get(
+        model_run_results_url,
+        params={"limit": "0"},
+        headers={"Content-Type": "application/json"},
+        cookies=cookies,
+    )
+
+    existing_model_run = None
+    for model_run in model_runs_result.json().get("results", ()):
+        if (
+            model_run["title"] == title
+            and model_run["performer"]["short_code"] == performer_shortcode
+            and "region" in model_run
+            and model_run["region"] is not None
+            and model_run["region"].get("name") == region_id
+        ):  # noqa
+            existing_model_run = model_run
+            break
+
+    if existing_model_run is not None:
+        model_run_id = model_run["id"]
+    else:
+        post_model_url = f"{rgd_endpoint}/api/scoring/model-runs/"
+
+        post_model_data = {
+            "performer": performer_shortcode,
+            "title": title,
+            "region": {"name": region_id},
+            "parameters": {},
+        }
+        if expiration_time is not None:
+            post_model_data["expiration_time"] = expiration_time
+        if eval_num is not None:
+            post_model_data["evaluation"] = eval_num
+        if eval_run_num is not None:
+            post_model_data["evaluation_run"] = eval_run_num
+        if proposal is True:
+            post_model_data["proposal"] = True
+        if title == "Ground Truth":
+            post_model_data["parameters"] = {"ground_truth": True}
+
+        post_model_result = requests.post(
+            post_model_url,
+            json=post_model_data,
+            headers={"Content-Type": "application/json"},
+            cookies=cookies,
+        )
+        model_run_id = post_model_result.json()["id"]
+
+    post_site_url = f"{rgd_endpoint}/api/scoring/model-runs/{model_run_id}/site-model/"
+    print(site_models_glob)
+    for site_file in glob(site_models_glob):
+        print("Uploading '{}' ..".format(site_file))
+        try:
+            result = post_site(post_site_url, site_file, rgd_auth_cookie)
+            print(result)
+        except Exception:
+            print("Exception occurred (printed below)")
+            traceback.print_exception(*sys.exc_info())
+        else:
+            if result.status_code != 201:
+                print(f"Error uploading site, status " f"code: [{result.status_code}]")
 
 
 if __name__ == "__main__":

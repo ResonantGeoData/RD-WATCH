4087ba02da4006c69096e5f5edb66c0fad4db828
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-11-08 13:58:02 -0500

    reverting model_run_uuid back to not allowing null values on the scoring app

--- django/src/rdwatch_scoring/models/satellite_fetching.py
+++ django/src/rdwatch_scoring/models/satellite_fetching.py
@@ -5,4 +5,4 @@ from rdwatch.models.satellite_fetching import BaseSatelliteFetching
 
 class SatelliteFetching(BaseSatelliteFetching):
     site = models.CharField(max_length=255)
-    model_run_uuid = models.CharField(max_length=255, blank=True, null=True)
+    model_run_uuid = models.CharField(max_length=255)

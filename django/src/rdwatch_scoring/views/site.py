dcf72f54de751babf65c697e36013e6e1b860c0c
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-11-21 15:14:08 -0500

    Refactor scoring site details endpoint in the same way

--- django/src/rdwatch_scoring/views/site.py
+++ django/src/rdwatch_scoring/views/site.py
@@ -1,39 +1,61 @@
+from datetime import datetime
+
 from ninja import Router
 from pydantic import UUID4
 
-from django.db.models import CharField, ExpressionWrapper, F, Value
-from django.db.models.functions import Concat, Substr
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 
-from rdwatch.db.functions import ExtractEpoch
-from rdwatch.views.site import SiteImageSiteDetailResponse
+from rdwatch.views.site_evaluation import SiteDetailResponse as BaseSiteDetailResponse
 from rdwatch_scoring.models import Site
 
 router = Router()
 
 
-@router.get('/{id}/details', response=SiteImageSiteDetailResponse)
-def siteDetails(request: HttpRequest, id: UUID4):
-    return (
-        Site.objects.filter(pk=id)
-        .annotate(
-            configurationId=F('evaluation_run_uuid'),
-            title=ExpressionWrapper(
-                Concat(
-                    Value('Eval '),
-                    F('evaluation_run_uuid__evaluation_number'),
-                    Value(' '),
-                    F('evaluation_run_uuid__evaluation_run_number'),
-                    Value(' '),
-                    F('evaluation_run_uuid__performer'),
-                ),
-                output_field=CharField(),
-            ),
-            timemin=ExtractEpoch('start_date'),
-            timemax=ExtractEpoch('end_date'),
-            regionName=F('region'),
-            performer=F('originator'),
-            siteNumber=Substr(F('site_id'), 9),  # pos is 1 indexed
+class SiteDetailResponse(BaseSiteDetailResponse):
+    @staticmethod
+    def resolve_configuration_id(obj: Site) -> str:
+        return str(obj.evaluation_run_uuid_id)
+
+    @staticmethod
+    def resolve_region_name(obj: Site) -> str:
+        return obj.region.id
+
+    @staticmethod
+    def resolve_title(obj: Site) -> str:
+        return ' '.join(
+            [
+                'Eval',
+                str(obj.evaluation_run_uuid.evaluation_number),
+                str(obj.evaluation_run_uuid.evaluation_run_number),
+                obj.evaluation_run_uuid.performer,
+            ]
         )
-        .first()
+
+    @staticmethod
+    def resolve_performer(obj: Site) -> str:
+        return obj.originator
+
+    @staticmethod
+    def resolve_timemin(obj: Site) -> int | None:
+        if obj.start_date:
+            return datetime.combine(obj.start_date, datetime.min.time()).timestamp()
+        return None
+
+    @staticmethod
+    def resolve_timemax(obj: Site) -> int | None:
+        if obj.end_date:
+            return datetime.combine(obj.end_date, datetime.min.time()).timestamp()
+        return None
+
+    @staticmethod
+    def resolve_number(obj: Site) -> str:
+        return obj.site_id[8:]
+
+
+@router.get('/{id}/', response=SiteDetailResponse)
+def get_site(request: HttpRequest, id: UUID4):
+    return get_object_or_404(
+        Site.objects.select_related('evaluation_run_uuid', 'region'),
+        pk=id,
     )

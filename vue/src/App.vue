5a62c0af30380a38cdd65294613b7ece1373a38b
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2023-11-21 15:27:08 -0500

    Add global error handler/popup snackbar (#320)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import ErrorPopup from './components/ErrorPopup.vue';
+
+import { onErrorCaptured, ref } from "vue";
+
+const errorText = ref('');
+
+onErrorCaptured((err) => {
+  errorText.value = [err.name, err.message, err.stack, err.cause].join('\n\n');
+  throw err;
+});
 </script>
 
 <template>
-  <v-app
-    id="RGD"
-  >
-    <router-view />
-  </v-app>
+  <div>
+    <ErrorPopup
+      :error-text="errorText"
+      @close="errorText = ''"
+    />
+    <v-app id="RGD">
+      <router-view />
+    </v-app>
+  </div>
 </template>

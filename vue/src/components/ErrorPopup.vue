dea729055ba12ba12831c488cf33d159fab0ce2a
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-05 11:08:08 -0500

    Redesign error popup UI

--- vue/src/components/ErrorPopup.vue
+++ vue/src/components/ErrorPopup.vue
@@ -13,52 +13,61 @@ function copyToClipboard(text: string) {
 </script>
 
 <template>
-  <v-snackbar
-    absolute
-    multi-line
-    location="top right"
-    :model-value="!!errorText"
-    vertical
-    :timeout="-1"
-    color="error"
-  >
-    <div class="mb-5">
-      <span class="text-h5">An error has occurred.</span>
-      <br>
-      <span class="text-body-1">If you'd like to file a bug report, please copy the error trace below by clicking the copy button and include it in your report.</span>
-    </div>
-    <v-divider />
-    <div class="d-flex">
-      <div class="d-flex align-center mr-3">
-        <v-tooltip
-          text="Copy to clipboard"
-          location="start"
-        >
-          <template #activator="{ props }">
-            <v-btn
-              v-bind="props"
-              size="x-large"
-              icon="mdi-content-copy"
-              @click="copyToClipboard(errorText)"
-            />
-          </template>
-        </v-tooltip>
-      </div>
-      <div
-        class="overflow-auto"
-        style="max-height: 40vh; white-space: pre-line;"
-      >
-        {{ errorText }}
-      </div>
-    </div>
-    <template #actions>
+  <v-dialog width="500">
+    <template #activator="{ props }">
       <v-btn
-        color="info"
-        variant="flat"
-        @click="emit('close')"
-      >
-        Close
-      </v-btn>
+        v-bind="props"
+        icon="mdi-alert"
+        location="top right"
+        position="absolute"
+        class="ma-4"
+        :color="errorText ? 'warning' : 'default'"
+        style="z-index: 2;"
+        :disabled="!errorText"
+      />
     </template>
-  </v-snackbar>
+
+    <template #default="{ isActive }">
+      <v-card title="Error">
+        <v-card-text>
+          <div class="mb-5">
+            <span class="text-body-1">If you'd like to file a bug report, please copy the error trace below by clicking the copy button and include it in your report.</span>
+          </div>
+          <v-divider />
+          <div class="d-flex">
+            <div class="d-flex align-center mr-3">
+              <v-tooltip
+                text="Copy to clipboard"
+                location="start"
+              >
+                <template #activator="{ props }">
+                  <v-btn
+                    v-bind="props"
+                    size="x-large"
+                    icon="mdi-content-copy"
+                    @click="copyToClipboard(errorText)"
+                  />
+                </template>
+              </v-tooltip>
+            </div>
+            <div
+              class="overflow-auto"
+              style="max-height: 40vh; white-space: pre-line;"
+            >
+              {{ errorText }}
+            </div>
+          </div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+
+          <v-btn
+            text="Close"
+            @click="isActive.value = false; emit('close')"
+          />
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
 </template>

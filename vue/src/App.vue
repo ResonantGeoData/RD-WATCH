3598a76d7e5273965f03724d736703e17a30d4fe
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-05 11:36:55 -0500

    Capture all error info in error handler
    Before, we weren't capturing some info, like response bodies in the case
    of a failed network request.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -6,7 +6,11 @@ import { onErrorCaptured, ref } from "vue";
 const errorText = ref('');
 
 onErrorCaptured((err) => {
-  errorText.value = [err.name, err.message, err.stack, err.cause].join('\n\n');
+  const error: Record<string, string> = {};
+  for (const [key, val] of Object.entries(err)) {
+    error[key] = JSON.stringify(val);
+  }
+  errorText.value = JSON.stringify(error);
   throw err;
 });
 </script>

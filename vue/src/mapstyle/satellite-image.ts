6bbf1638050d6acb34a94a60c2b779fa382f7db2
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2023-12-06 15:11:24 -0500

    Fix crash if exact timestamp is not found in region satellite image list (#333)

--- vue/src/mapstyle/satellite-image.ts
+++ vue/src/mapstyle/satellite-image.ts
@@ -51,7 +51,7 @@ export const buildSourceFilter = (
     }
     return undefined;
 }
-  
+
 export const layers = (
     timestamp: number,
     satellite: SatelliteData
@@ -67,8 +67,8 @@ export const layers = (
           paint: {
               "raster-opacity":satellite.imageOpacity,
               "raster-fade-duration": 0,
-            },      
-      },  
+            },
+      },
     ];
     return layers;
   }
@@ -88,10 +88,16 @@ export const setSatelliteTimeStamp = (state: { filters: MapFilters, satellite: S
         return distanceA - distanceB;
     })
     // Lets try to get the closes timestamp that is less than the current time.
-    const date = baseList[0];
-    const timeStamp = date.toISOString().substring(0,19);
-    const currentIndex = state.satellite.satelliteTimeList.findIndex((item) => item.timestamp === timeStamp);
-    state.satellite.satelliteTimeSource = state.satellite.satelliteTimeList[currentIndex].source;
-    state.satellite.satelliteTimeStamp = timeStamp;
-}
+    // Try each date in the list until one works.
+    for (const date of baseList) {
+      const timeStamp = date.toISOString().substring(0,19);
+      const currentIndex = state.satellite.satelliteTimeList.findIndex((item) => item.timestamp === timeStamp);
+      if (currentIndex === -1) {
+        continue;
+      }
+      state.satellite.satelliteTimeSource = state.satellite.satelliteTimeList[currentIndex].source;
+      state.satellite.satelliteTimeStamp = timeStamp;
+      break;
+    }
+  }
 }

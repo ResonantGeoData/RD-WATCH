d147c98a3a76e01c1939d272debdc5a401424550
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-12-13 09:06:48 -0500

    support for higher number of regions

--- django/src/rdwatch/views/region.py
+++ django/src/rdwatch/views/region.py
@@ -1,12 +1,14 @@
-from ninja.pagination import RouterPaginated
+from ninja import Router
+from ninja.pagination import PageNumberPagination, paginate
 
 from django.http import HttpRequest
 
 from rdwatch.models import Region
 
-router = RouterPaginated()
+router = Router()
 
 
 @router.get('/', response=list[str])
+@paginate(PageNumberPagination, page_size=1000)
 def list_regions(request: HttpRequest):
     return Region.objects.values_list('name', flat=True)

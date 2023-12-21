8fa2481dcc4f65886d466eb3f552bb1692b57d23
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2023-12-21 11:16:38 -0500

    Update vue/vite.config.ts
    Co-authored-by: Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -5,16 +5,16 @@ import { execSync } from "child_process";
 export default ({ mode}) => {
   // Loads all environment variables to see if we are in VITE_DOCKER_DEVELOPMENT
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const commitDate = execSync("git log -1 --format=%cI").toString().trimEnd();
-    const branchName = execSync("git rev-parse --abbrev-ref HEAD")
-      .toString()
-      .trimEnd();
-    const commitHash = execSync("git rev-parse HEAD").toString().trimEnd();
-    const lastCommitMessage = execSync("git show -s --format=%s")
-      .toString()
-      .trimEnd();
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const commitDate = execSync("git log -1 --format=%cI").toString().trimEnd();
+  const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+    .toString()
+    .trimEnd();
+  const commitHash = execSync("git rev-parse HEAD").toString().trimEnd();
+  const lastCommitMessage = execSync("git show -s --format=%s")
+    .toString()
+    .trimEnd();
 
     process.env.VITE_GIT_COMMIT_DATE = commitDate;
     process.env.VITE_GIT_BRANCH_NAME = branchName;

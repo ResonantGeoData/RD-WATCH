8f7bcc39b649519c62d107c3a2f3b69b06f6436a
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-03-13 16:13:52 -0400

    Include version in server status endpoint, swagger page

--- django/src/rdwatch/server/settings.py
+++ django/src/rdwatch/server/settings.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 from datetime import timedelta
+from pathlib import Path
 
 from configurations import Configuration, values
 
@@ -11,6 +13,12 @@ _ENVIRON_PREFIX = 'RDWATCH'
 # classes (e.g. `AWS_DEFAULT_REGION) get immediately evaluated and
 # expect env vars to be set.
 values.Value.late_binding = True
+
+GIT_VERSION = subprocess.check_output(
+    ['git', 'describe', '--tags'],
+    encoding='utf-8',
+    cwd=str(Path(__file__).parents[4].resolve()),
+).strip()
 
 
 class BaseConfiguration(Configuration):
@@ -24,6 +32,8 @@ class BaseConfiguration(Configuration):
     # Django's docs suggest that STATIC_URL should be a relative path,
     # for convenience serving a site on a subpath.
     STATIC_URL = 'static/'
+
+    GIT_VERSION = GIT_VERSION
 
     @property
     def INSTALLED_APPS(self):

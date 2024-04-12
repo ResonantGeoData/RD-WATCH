6a7de188d3270a822ab56e0a21197518b6955897
JHUAPL <>
2024-04-12 15:37:14 -0400

    Initial commit for proposals with scoring db

--- django/src/rdwatch_scoring/models/__init__.py
+++ django/src/rdwatch_scoring/models/__init__.py
@@ -1,10 +1,8 @@
-from .region import Region
-from .annotation_ground_truth_site import AnnotationGroundTruthSite
-from .annotation_ground_truth_observation import AnnotationGroundTruthObservation
-from .annotation_proposal_set import AnnotationProposalSet
-from .annotation_proposal_site_log import AnnotationProposalSiteLog
-from .annotation_proposal_site import AnnotationProposalSite
-from .annotation_proposal_observation import AnnotationProposalObservation
+from .region import Region  # isort:skip
+from .annotation_ground_truth_site import AnnotationGroundTruthSite  # isort:skip
+from .annotation_ground_truth_observation import (
+    AnnotationGroundTruthObservation,  # isort:skip
+)
 from .evaluation_activity_classification_f1 import EvaluationActivityClassificationF1
 from .evaluation_activity_classification_matrix import (
     EvaluationActivityClassificationMatrix,
@@ -31,6 +29,11 @@ from .performer import Performer
 from .satellite_fetching import SatelliteFetching
 from .site import Site
 from .site_image import SiteImage
+
+from .annotation_proposal_set import AnnotationProposalSet  # isort:skip
+from .annotation_proposal_site import AnnotationProposalSite  # isort:skip
+from .annotation_proposal_site_log import AnnotationProposalSiteLog  # isort:skip
+from .annotation_proposal_observation import AnnotationProposalObservation  # isort:skip
 
 __all__ = [
     'Region',

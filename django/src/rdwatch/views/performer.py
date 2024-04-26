ab315c05c12640a83e98b50f6ac4116193144999
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-04-26 11:50:34 -0400

    Add endpoint for creating a performer

--- django/src/rdwatch/views/performer.py
+++ django/src/rdwatch/views/performer.py
@@ -1,6 +1,7 @@
 from ninja import Schema
 from ninja.pagination import RouterPaginated
 
+from django.db.models import Q
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
 
@@ -9,6 +10,11 @@ from rdwatch.models import Performer
 
 class PerformerSchema(Schema):
     id: int
+    team_name: str
+    short_code: str
+
+
+class PerformerCreateSchema(Schema):
     team_name: str
     short_code: str
 
@@ -27,3 +33,13 @@ def get_performer(request: HttpRequest, id: int):
         Performer.objects.all(),
         id=id,
     )
+
+
+@router.post('/', response={201: PerformerSchema, 409: str})
+def create_performer(request: HttpRequest, payload: PerformerCreateSchema):
+    if Performer.objects.filter(
+        Q(team_name=payload.team_name) | Q(short_code=payload.short_code)
+    ).exists():
+        return 409, 'Performer already exists'
+
+    return 201, Performer.objects.create(**payload.dict())

59ab0a2ad7f3c05db359bb3684556adcc014c5f0
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2024-05-10 14:05:39 -0400

    Use `poetry-dynamic-versioning` plugin for versioning (#421)
    * Enable `poetry-dynamic-versioning` plugin
    
    * Configure poetry-dynamic-versioning version substitution
    
    * Replace GIT_VERSION setting with dynamic version

--- django/src/rdwatch/__init__.py
+++ django/src/rdwatch/__init__.py
@@ -2,3 +2,7 @@
 # is always defined prior to any shared_task definitions (so those tasks will bind to
 # the app), import the Celery module here for side effects.
 from .celery import app as _celery_app  # noqa: F401
+
+# placeholder for version number, will be replaced by
+# poetry-dynamic-versioning in production
+__version__ = '0.0.0dev0'

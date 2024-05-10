943b45fd8bcfcf8b62f44f5f78b8a631fb2ba3ff
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-05-10 13:39:27 -0400

    Replace GIT_VERSION setting with dynamic version

--- django/src/rdwatch/api.py
+++ django/src/rdwatch/api.py
@@ -1,7 +1,7 @@
 from ninja import NinjaAPI
 from ninja.errors import ValidationError
 
-from django.conf import settings
+from rdwatch import __version__ as rdwatch_version
 
 from .views import site
 from .views.model_run import router as model_run_router
@@ -13,7 +13,7 @@ from .views.site_evaluation import router as site_evaluation_router
 from .views.site_image import router as images_router
 from .views.site_observation import router as site_observation_router
 
-api = NinjaAPI(title='RD-WATCH', version=settings.GIT_VERSION)
+api = NinjaAPI(title='RD-WATCH', version=rdwatch_version)
 
 api.add_router('/evaluations/', site_evaluation_router)
 api.add_router('/observations/', site_observation_router)

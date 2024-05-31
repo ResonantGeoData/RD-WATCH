0748e280e78ecfe9e8eb0fbd26f15efaad4fb71b
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-05-30 20:31:06 -0400

    Put script section first in sfc

--- vue/src/views/SmartFlow.vue
+++ vue/src/views/SmartFlow.vue
@@ -1,3 +1,88 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { debounce } from 'lodash';
+import { SmartflowService } from '../client';
+
+interface DagRun {
+  dag_id: string;
+  dag_run_id: string;
+  data_interval_end: string;
+  data_interval_start: string;
+  end_date: string;
+  execution_date: string;
+  external_trigger: boolean;
+  last_scheduling_decision: string;
+  logical_date: string;
+  note: string | null;
+  run_type: string;
+  start_date: string;
+  state: string;
+}
+
+const headers = [
+  { title: 'DAG ID', key: 'dag_id' },
+  { title: 'DAG Run ID', key: 'dag_run_id' },
+  { title: 'Status', key: 'state' },
+  { title: 'Actions', key: 'actions' },
+];
+
+const dag = ref('');
+const dagRuns = ref<DagRun[]>([]);
+const itemsPerPage = ref(10);
+const totalItems = ref(0);
+const loading = ref(false);
+
+const dagSearch = ref('');
+const dagResults = ref<string[]>([]);
+
+const newDagRunDialog = ref<boolean>(false);
+const newDagRunTitle = ref<string>('');
+const newDagRunConf = ref<string>('');
+
+const fetchDags = debounce(async (searchTerm: string) => {
+  loading.value = true;
+  const query = searchTerm ? { dag_id_pattern: searchTerm } : {};
+  const res = await SmartflowService.rdwatchSmartflowViewsListDags(query);
+  dagResults.value = res.dags.map((dag: any) => dag.dag_id);
+  loading.value = false;
+}, 500);
+
+watch(dagSearch, fetchDags);
+
+const fetchDagRuns = async ({ page = 1, itemsPerPage = 10 }) => {
+  loading.value = true;
+  const { dag_runs, total_entries } = await SmartflowService.rdwatchSmartflowViewsListDagRuns(dag.value, { limit: itemsPerPage, offset: (page - 1) * itemsPerPage });
+  totalItems.value = total_entries;
+  dagRuns.value = dag_runs;
+  loading.value = false;
+};
+
+watch(dag, () => fetchDagRuns({ page: 1, itemsPerPage: itemsPerPage.value }));
+
+const stateColor = (state: string): string => {
+  switch (state) {
+    case 'success':
+      return 'green';
+    case 'failed':
+      return 'red';
+    default:
+      return 'grey';
+  }
+};
+
+const showNewDagRunDialog = () => {
+  newDagRunDialog.value = true;
+};
+
+const startNewRun = async () => {
+  // Placeholder for starting a new run with selected region
+  await SmartflowService.rdwatchSmartflowViewsCreateDagRun(dag.value, { dag_run_title: newDagRunTitle.value, conf: JSON.parse(newDagRunConf.value)});
+  newDagRunDialog.value = false;
+};
+
+onMounted(() => fetchDags);
+</script>
+
 <template>
   <v-container>
     <v-card>
@@ -96,88 +181,3 @@
     </v-card>
   </v-container>
 </template>
-
-<script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import { debounce } from 'lodash';
-import { SmartflowService } from '../client';
-
-interface DagRun {
-  dag_id: string;
-  dag_run_id: string;
-  data_interval_end: string;
-  data_interval_start: string;
-  end_date: string;
-  execution_date: string;
-  external_trigger: boolean;
-  last_scheduling_decision: string;
-  logical_date: string;
-  note: string | null;
-  run_type: string;
-  start_date: string;
-  state: string;
-}
-
-const headers = [
-  { title: 'DAG ID', key: 'dag_id' },
-  { title: 'DAG Run ID', key: 'dag_run_id' },
-  { title: 'Status', key: 'state' },
-  { title: 'Actions', key: 'actions' },
-];
-
-const dag = ref('');
-const dagRuns = ref<DagRun[]>([]);
-const itemsPerPage = ref(10);
-const totalItems = ref(0);
-const loading = ref(false);
-
-const dagSearch = ref('');
-const dagResults = ref<string[]>([]);
-
-const newDagRunDialog = ref<boolean>(false);
-const newDagRunTitle = ref<string>('');
-const newDagRunConf = ref<string>('');
-
-const fetchDags = debounce(async (searchTerm: string) => {
-  loading.value = true;
-  const query = searchTerm ? { dag_id_pattern: searchTerm } : {};
-  const res = await SmartflowService.rdwatchSmartflowViewsListDags(query);
-  dagResults.value = res.dags.map((dag: any) => dag.dag_id);
-  loading.value = false;
-}, 500);
-
-watch(dagSearch, fetchDags);
-
-const fetchDagRuns = async ({ page = 1, itemsPerPage = 10 }) => {
-  loading.value = true;
-  const { dag_runs, total_entries } = await SmartflowService.rdwatchSmartflowViewsListDagRuns(dag.value, { limit: itemsPerPage, offset: (page - 1) * itemsPerPage });
-  totalItems.value = total_entries;
-  dagRuns.value = dag_runs;
-  loading.value = false;
-};
-
-watch(dag, () => fetchDagRuns({ page: 1, itemsPerPage: itemsPerPage.value }));
-
-const stateColor = (state: string): string => {
-  switch (state) {
-    case 'success':
-      return 'green';
-    case 'failed':
-      return 'red';
-    default:
-      return 'grey';
-  }
-};
-
-const showNewDagRunDialog = () => {
-  newDagRunDialog.value = true;
-};
-
-const startNewRun = async () => {
-  // Placeholder for starting a new run with selected region
-  await SmartflowService.rdwatchSmartflowViewsCreateDagRun(dag.value, { dag_run_title: newDagRunTitle.value, conf: JSON.parse(newDagRunConf.value)});
-  newDagRunDialog.value = false;
-};
-
-onMounted(() => fetchDags);
-</script>

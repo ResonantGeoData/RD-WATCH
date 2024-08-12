4968c6d062b28a1b26e85b86b9c06832d72bdbdc
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-08-12 12:51:01 -0400

    Parse version string + format it in status endpoint

--- rdwatch/core/views/server_status.py
+++ rdwatch/core/views/server_status.py
@@ -2,6 +2,7 @@ import datetime
 import socket
 from typing import Any
 
+import semver
 from ninja import Field, Router, Schema
 
 from django.http import HttpRequest
@@ -39,6 +40,15 @@ def get_status(request: HttpRequest):
     uptime = datetime.datetime.now() - SERVER_INSTANCE_EPOCH
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
+    parsed_version = semver.Version.parse(api.version)
+
+    # use pattern "v{base}{distance:+.dev+} ({commit:.7})"
+    formatted_version = (
+        f'v{parsed_version.major}.{parsed_version.minor}.{parsed_version.patch}'
+    )
+    if parsed_version.prerelease:
+        formatted_version += f'+{parsed_version.prerelease}'
+    formatted_version += f' ({parsed_version.build})'
 
     try:
         smartflow_status = SmartFlowClient().get_health().to_dict()
@@ -49,6 +59,6 @@ def get_status(request: HttpRequest):
         uptime=uptime,
         hostname=hostname,
         ip=ip,
-        rdwatch_version=api.version,
+        rdwatch_version=formatted_version,
         smartflow=smartflow_status,
     )

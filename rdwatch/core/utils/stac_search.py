9270a851eff49be24f2fcdf345470b7fdab309a4
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-09-04 14:12:37 -0400

    Hardcode element84 STAC server

--- rdwatch/core/utils/stac_search.py
+++ rdwatch/core/utils/stac_search.py
@@ -4,8 +4,6 @@ from typing import Literal, TypedDict, cast
 
 from pystac_client import Client
 
-from django.conf import settings
-
 logger = logging.getLogger(__name__)
 
 
@@ -52,22 +50,11 @@ def _fmt_time(time: datetime):
 
 COLLECTIONS: dict[str, list[str]] = {
     'L8': [],
-    'S2': [],
+    'S2': ['sentinel-2-c1-l2a', 'sentinel-2-l2a'],
     'PL': [],
 }
 
-STAC_URL = ''
-STAC_HEADERS = {}
-if settings.STAC_URL:
-    COLLECTIONS['S2'] += ['sentinel-2-c1-l2a', 'sentinel-2-l2a']
-    COLLECTIONS['L8'].append('landsat-c2-l2')
-    STAC_URL = settings.STAC_URL
-elif settings.settings.ACCENTURE_VERSION is not None:
-    COLLECTIONS['S2'].append(f'ta1-s2-acc-{settings.ACCENTURE_VERSION}')
-    COLLECTIONS['L8'].append(f'ta1-ls-acc-{settings.ACCENTURE_VERSION}')
-    COLLECTIONS['PL'].append(f'ta1-pd-acc-{settings.ACCENTURE_VERSION}')
-    STAC_URL = settings.SMART_STAC_URL
-    STAC_HEADERS = {'x-api-key': settings.SMART_STAC_KEY}
+STAC_URL = 'https://earth-search.aws.element84.com/v1/'
 
 
 def stac_search(
@@ -76,7 +63,7 @@ def stac_search(
     bbox: tuple[float, float, float, float],
     timebuffer: timedelta | None = None,
 ) -> Results:
-    stac_catalog = Client.open(STAC_URL, headers=STAC_HEADERS)
+    stac_catalog = Client.open(STAC_URL)
 
     if timebuffer is not None:
         min_time = timestamp - timebuffer
@@ -84,11 +71,7 @@ def stac_search(
         time_str = f'{_fmt_time(min_time)}/{_fmt_time(max_time)}'
     else:
         time_str = f'{_fmt_time(timestamp)}Z'
-    if source not in COLLECTIONS:
-        logger.warning(
-            'Source {source} not in Collections: {COLLECTIONS} returning empty list'
-        )
-        return {'features': [], 'links': []}
+
     results = stac_catalog.search(
         method='GET',
         bbox=bbox,
@@ -96,6 +79,7 @@ def stac_search(
         collections=COLLECTIONS[source],
         limit=100,
     )
+
     # TODO: return `results` directly instead of converting to a dict.
     # Before that can happen, the callers need to be updated to handle
     # an `ItemSearch` object.

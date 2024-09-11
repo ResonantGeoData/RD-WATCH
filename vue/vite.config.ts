205d7f2184021569b4747fbe3905ef66fa2cd282
Bryon Lewis <Bryon.Lewis@kitware.com>
2024-09-11 12:06:17 -0400

    reverting vite config change

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -71,10 +71,6 @@ export default ({ mode}) => {
             target: `http://${devHost}:8000`,
             xfwd: true,
           },
-          "/static": {
-            target: `http://${devHost}:8000`,
-            xfwd: true,
-          },
         },
         strictPort: true,
       },

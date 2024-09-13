f363af54b9559807c80a07a7bc786ff56453b1c0
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2024-09-13 09:12:36 -0400

    set timeMin to modelRun list timeMin (#506)

--- vue/src/components/annotationViewer/SideBar.vue
+++ vue/src/components/annotationViewer/SideBar.vue
@@ -11,7 +11,6 @@ import type { Ref } from "vue";
 import { changeTime } from "../../interactions/timeStepper";
 import ErrorPopup from "../ErrorPopup.vue";
 import ModeSelector from "../ModeSelector.vue";
-const timemin = ref(Math.floor(new Date(0).valueOf() / 1000));
 
 const queryFilters = computed<QueryArguments>(() => ({
   performer: selectedPerformer.value.map((item) => item.short_code),
@@ -150,7 +149,7 @@ const satelliteLoadingColor = computed(() => {
       <mode-selector />
       <v-row>
         <TimeSlider
-          :min="timemin || 0"
+          :min="state.timeMin"
           :max="Math.floor(Date.now() / 1000)"
         />
       </v-row>

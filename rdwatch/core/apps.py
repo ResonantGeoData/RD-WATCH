e5217d5eb83a2bc900b4eed9b80dc8b09005a8a1
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2024-11-01 10:11:57 -0400

    Enable Sentry performance tracking (#530)

--- rdwatch/core/apps.py
+++ rdwatch/core/apps.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
@@ -12,6 +13,25 @@ from django.conf import settings
 class RDWatchConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'rdwatch.core'
+
+    @staticmethod
+    def _get_sentry_trace_sample_rate(*args, **kwargs) -> float:
+        # Routes that should have a 100% trace rate.
+        # All other routes will have a 5% trace rate.
+        priority_routes = [
+            r'^/api/scoring/model-runs/[^/]+/sites/$',
+        ]
+
+        if len(args) and 'wsgi_environ' in args[0]:
+            wsgi_environ = args[0]['wsgi_environ']
+
+            url_path = wsgi_environ.get('PATH_INFO')
+
+            for regex in priority_routes:
+                if re.match(regex, url_path):
+                    return 1.0
+
+        return 0.05
 
     def ready(self) -> None:
         if hasattr(settings, 'SENTRY_DSN'):
@@ -30,7 +50,7 @@ class RDWatchConfig(AppConfig):
                 attach_stacktrace=True,
                 # Submit request User info from Django
                 send_default_pii=True,
-                traces_sampler=0.01,
-                profiles_sampler=0.01,
+                traces_sampler=self._get_sentry_trace_sample_rate,
+                profiles_sample_rate=1.0,
             )
         return super().ready()
